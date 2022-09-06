2867fe32ed2be6d16545fe78035ff5caf6478a14
VAOkolo <vincentaokolo@outlook.com>
2022-09-06 16:35:24 +0100

    third commit

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const { seedDB } = require("./seeds");
 const PORT = process.env.PORT || 7000;
 
-mongoose
-  .connect(process.env.MONG_URI)
-//   .then(() => {
-//       seedDB()
-//   })
+mongoose.connect(process.env.MONG_URI)
   .then(() => {
     server.listen(PORT, () => {
       console.log("Connected to db & listening at port " + PORT);
